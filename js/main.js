e85df727eae1900ebf30529452d62c20538d4aa4
gordenia <gordenia@inbox.ru>
2017-07-29 16:44:18 +0300

    Init

--- js/main.js
+++ js/main.js
@@ -1,17 +1,44 @@
-$(document).ready(function(){
-    smoothScroll();
-    gallery();
-});
-
-function smoothScroll(){
-    $('a.menu-item').click(function (){
+$(document).ready(function()
+{
+    $('a.menu-item').click(function ()
+    {
         $('html, body').animate({scrollTop: $($(this).attr('href')).position().top - 60}, 2000);
     });
-}
 
-function gallery() {
-    $('.gallery').lightGallery({
+    $('.gallery').lightGallery
+    ({
         selector: 'a'
     });
-}
+
+    $(".input-file").before(
+        function()
+        {
+            if ( ! $(this).prev().hasClass('input-ghost') )
+            {
+                var element = $("<input type='file' class='input-ghost' style='visibility:hidden; height:0'>");
+                element.attr("name",$(this).attr("name"));
+                element.change(function()
+                {
+                    element.next(element).find('input').val((element.val()).split('\\').pop());
+                });
+                $(this).find("button.btn-choose").click(function()
+                {
+                    element.click();
+                });
+                $(this).find("button.btn-reset").click(function()
+                {
+                    element.val(null);
+                    $(this).parents(".input-file").find('input').val('');
+                });
+                $(this).find('input').css("cursor","pointer");
+                $(this).find('input').mousedown(function()
+                {
+                     $(this).parents('.input-file').prev().click();
+                     return false;
+                });
+                return element;
+            }
+        });
+});
+
 
